f7a96cfa2d69c035fbb0ff8349a3ac5fd52187ac
Andy Footner <andyfootner@netscape.net>
2023-09-14 14:22:46 +0200

    chore: align local set up with standard
    Refs: OPS-9227

--- local/install.sh
+++ local/install.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-#
-# Wrapper to help spin up a local Drupal site.
-#
-# The preferred way to get started is to install Drupal from the "minimal" install profile
-# with an empty database and load all configuration from the repository:
-#
-#   ./local/install.sh -i -c
-#
-# Which is equivalent to:
-#
-#   drush -y si --existing-config minimal install_configure_form.enable_update_status_emails=NULL
 
 set -e -u
 
@@ -20,7 +9,7 @@ usage() {
   echo "-m                    : Create local image" >&2
   echo "-i                    : Install site" >&2
   echo "-c                    : Use existing config to install site" >&2
-  echo "-p network            : Indicate the local proxy network name (defaults to 'proxy')" >&2
+  echo "-d                    : Install dev dependencies" >&2
   echo "-x                    : Shutdown and remove the site containers" >&2
   echo "-v                    : Also remove the volumes when shutting down the containers" >&2
   exit 1
@@ -29,12 +18,12 @@ usage() {
 create_image="no"
 install_site="no"
 use_existing_config="no"
-proxy_name="proxy"
+install_dev_dependencies="no"
 shutdown="no"
 shutdown_options=""
 
 # Parse options.
-while getopts "hmicp:xv" opt; do
+while getopts "hmicdxv" opt; do
   case $opt in
     h)
       usage
@@ -48,8 +37,8 @@ while getopts "hmicp:xv" opt; do
     c)
       use_existing_config="yes"
       ;;
-    p)
-      proxy_name="${OPTARG//,/ }"
+    d)
+      install_dev_dependencies="yes"
       ;;
     x)
       shutdown="yes"
@@ -63,10 +52,18 @@ while getopts "hmicp:xv" opt; do
   esac
 done
 
+function docker_compose {
+  docker compose -f local/docker-compose.yml "$@"
+}
+
+# Load the environment variables.
+# They are only available in this script as we don't export them.
+source local/.env
+
 # Stop and remove the containers.
 if [ "$shutdown" = "yes" ]; then
   echo "Stop and remove the containers."
-  PROXY=$proxy_name docker-compose -p starterkit-local -f local/docker-compose.yml down $shutdown_options || true
+  docker_compose down $shutdown_options || true
   exit 0
 fi
 
@@ -74,40 +71,74 @@ fi
 # Build local image.
 if [ "$create_image" = "yes" ]; then
   echo "Build local image."
-  make
+  make IMAGE_NAME=$IMAGE_NAME IMAGE_TAG=$IMAGE_TAG
 fi;
 
-# Create the site, memcache and mysql containers.
-echo "Create the site, memcache and mysql containers."
-PROXY=$proxy_name docker-compose -p starterkit-local -f local/docker-compose.yml up -d
+# Create the site and mysql containers.
+echo "Create the site and mysql containers."
+docker_compose up -d --remove-orphans
+
+# Wait a bit for  mysql to be ready.
+echo "Wait a bit for mysql to be ready."
+sleep 10
 
 # Dump some information about the created containers.
 echo "Dump some information about the created containers."
-docker ps -a -fname=starterkit-local
-
-# Wait a bit for memcache and mysql to be ready.
-echo "Wait a bit for memcache and mysql to be ready."
-sleep 10
+docker_compose ps -a
 
 # Install the site.
 if [ "$install_site" = "yes" ]; then
   # Ensure the file directories are writable.
   echo "Ensure the file directories are writable."
-  docker exec -it starterkit-local-site chmod -R 777 /srv/www/html/sites/default/files /srv/www/html/sites/default/private
+  docker_compose exec site chmod -R 777 /srv/www/html/sites/default/files /srv/www/html/sites/default/private
 
-  # Install the dev dependencies.
-  echo "Install the common design subtheme if not present already"
-  docker exec -it -w /srv/www starterkit-local-site composer run sub-theme
+  # Copy the existing settings.php and ensure the settings.php file is writable.
+  echo "Ensure the settings.php writable."
+  docker_compose exec site sh -c "cp /srv/www/html/sites/default/settings.php /srv/www/html/sites/default/settings.php.backup"
+  docker_compose exec site chmod 666 /srv/www/html/sites/default/settings.php
+
+  # Install the subtheme.
+  echo "Install the common design subtheme if not present already."
+  docker_compose exec -w /srv/www site composer run sub-theme || true
 
   # Install the site with the existing config.
   if [ "$use_existing_config" = "yes" ]; then
     echo "Install the site with the existing config."
-    docker exec -it starterkit-local-site drush -y si --existing-config minimal install_configure_form.enable_update_status_emails=NULL
+    docker_compose exec -u appuser site drush -y si --existing-config minimal install_configure_form.enable_update_status_emails=NULL
   else
     echo "Install the site from scratch."
-    docker exec -it starterkit-local-site drush -y si minimal install_configure_form.enable_update_status_emails=NULL
+    docker_compose exec -u appuser site drush -y si minimal install_configure_form.enable_update_status_emails=NULL
   fi
 
   # Import the configuration.
-  docker exec -it starterkit-local-site drush -y cim
+  docker_compose exec -u appuser site drush -y cim
+
+  # Restore the our copy of the settings.php.
+  echo "Restore the settings.php."
+  docker_compose exec site sh -c "mv /srv/www/html/sites/default/settings.php.backup /srv/www/html/sites/default/settings.php"
+fi
+
+# Install the dev dependencies and re-import the configuration.
+if [ "$install_dev_dependencies" = "yes" ]; then
+  # Install the dev dependencies.
+  echo "Install the dev dependencies."
+  docker_compose exec -w /srv/www site composer install
+
+  # Import the configuration.
+  docker_compose exec -u appuser site drush -y cr
+
+  # Import the configuration.
+  docker_compose exec  -u appuser site drush -y updatedb --no-post-updates
+
+  # Import the configuration.
+  docker_compose exec  -u appuser site drush -y cim
+
+  # Import the configuration.
+  docker_compose exec  -u appuser site drush -y updatedb
+
+  # Enable the devel module.
+  docker_compose exec  -u appuser site drush -y en devel
+
+  # Enable the stage file proxy module.
+  docker_compose exec  -u appuser site drush -y en stage_file_proxy
 fi
